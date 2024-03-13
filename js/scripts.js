9c977e03dd73e6026082e6a186f8d9b43c5ea674
kareem reda <147895396+DevKareemReda@users.noreply.github.com>
2024-03-13 13:39:12 -0700

    update website, next once add dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,52 @@
+$(function () {
+    $('.owl-testmonials').owlCarousel({
+        loop:true,
+        autoplay: true,
+        margin:10,
+        items: 1,
+        nav:false,
+        dots: true,
+        smartSpeed: 800,
+    autoplayHoverPause:true
+    })
+})
+
 let headerNav = document.querySelector("#header-nav");
 document.body.style.paddingTop = headerNav.offsetHeight + "px";
 
-window.onscroll = function () {
+function scrollHeader() {
     headerNav.classList.toggle("sticky", this.scrollY > 150);
 }
+window.addEventListener("scroll", scrollHeader)
 
-let openDrop = document.querySelectorAll(".nav-mobile ul li a .fa-chevron-down");
-let navMobile = document.querySelector(".nav-mobile");
+let allLinkTo = document.querySelectorAll("main ul li a.link-to");
+allLinkTo.forEach(el => {
+    el.onclick = function (e) {
+        e.preventDefault();
+        allLinkTo.forEach(el => el.classList.remove("active"));
+        let toScroll = document.querySelector("#" + this.getAttribute("data-scroll")).offsetTop;
+        scrollTo({
+            top: toScroll - headerNav.offsetHeight,
+            behavior: 'smooth'
+        });
+        this.classList.add("active");
+    };
+});
+
+
+let openDrop = document.querySelectorAll("ul li a .fa-chevron-down");
+let navMobile = document.querySelector("main");
 let headerBars = document.querySelector("#header-nav .fa-bars")
 let overlayBody = document.querySelector(".overlay-body")
 let closeNav = document.querySelector(".mobile-site-close")
+
 openDrop.forEach(el => {
     el.onclick = function (e) {
         e.preventDefault();
-        this.classList.toggle("active");
-        if (this.classList.contains("active")) {
-            this.parentElement.parentElement.classList.add("active")
-        } else {
-            this.parentElement.parentElement.classList.remove("active")
-        }
+        el.parentElement.parentElement.classList.toggle("active")
+        el.parentElement.parentElement.classList.contains("active")
+            ? el.parentElement.parentElement.classList.add("active")
+            : el.parentElement.parentElement.classList.remove("active")
     }
 })
 
@@ -38,3 +66,36 @@ window.onclick = function (e) {
         overlayBody.classList.remove("active")
     }
 }
+
+let block = document.querySelectorAll(".block");
+function scrollLinks() {
+    block.forEach(el => {
+        if (this.scrollY >= el.offsetTop - headerNav.offsetHeight) {
+            allLinkTo.forEach(el => el.classList.remove("active"));
+            document.querySelector("ul li a[data-scroll ="+el.id+"]").classList.add("active")
+        }
+    })
+}
+window.addEventListener("scroll", scrollLinks)
+
+window.onresize = function () {
+    if (this.innerWidth >= 992) {
+        navMobile.classList.remove("active")
+        overlayBody.classList.remove("active")
+    }
+}
+
+let goUp = document.querySelector(".up");
+window.onscroll = function () {
+    goUp.classList.toggle('active', this.scrollY >= 550);
+}
+
+goUp.onclick = function () {
+    scrollTo ({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+let copyRight = document.querySelector(".copy-right span");
+copyRight.innerHTML = new Date().getFullYear()
